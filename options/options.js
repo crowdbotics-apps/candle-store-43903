75f854ecda571e1bbecc18803d4abe1e62705fab
Crowdbotics <team@crowdbotics.com>
2023-10-12 18:39:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "candle-store-43903",
+  url: "https://candle-store-43903.botics.co",
+  api: "https://candle-store-43903.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
